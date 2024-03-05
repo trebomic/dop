0fa9f21ac9607ba14b1b34e7b24f3d06b2ed877b
oehlemar <oehlemar@students.zhaw.ch>
2024-03-05 19:29:11 +0100

    changel line 7 in soll_ich_arbeiten.py

--- Woche 3 Versionskontrolle (Git)/soll_ich_arbeiten.py
+++ Woche 3 Versionskontrolle (Git)/soll_ich_arbeiten.py
@@ -4,7 +4,7 @@ def soll_ich_arbeiten():
     entscheidungen = [
         ("Ja, heute ist ein super produktiver Tag!", "Wenn nicht, gibts morgen dreifach soviel Kaffee!"),
         ("Nein, gönn dir eine Pause!", "Aber du wirst von dem Traum geplagt werden, in dem du versehentlich alle Emails löschst."),
-        ("Vielleicht etwas leichte Arbeit heute.", "Sonst findest du morgen eine zusätzliche Besprechung in deinem Kalender."),
+        ("Vielleicht etwas weniger leichte Arbeit heute.", "Sonst findest du morgen eine zusätzliche Besprechung in deinem Kalender."),
         ("Heute ist ein guter Tag, um etwas Neues zu lernen!", "Ignoriere es, und du wirst den ganzen Tag den Song 'Baby Shark' nicht loswerden."),
         ("Zeit für Kaffee, und dann entscheidest du.", "Kein Kaffee? Dann bereite dich auf den unendlichen Tee-Witz von deinen Kollegen vor.")
     ]

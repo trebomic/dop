73b6a7cbf5ffb4f233c7ba3b6ec3c5b3a1da0c6e
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-12 14:03:48 +0100

    Update App.java

--- Woche 4 Build Tools (Gradle und NPM)/MyFirstBuild/app/src/main/java/myfirstbuild/App.java
+++ Woche 4 Build Tools (Gradle und NPM)/MyFirstBuild/app/src/main/java/myfirstbuild/App.java
@@ -1,6 +1,8 @@
 package myfirstbuild;
 
+import com.github.lalyos.jfiglet.FigletFont;
 import org.apache.commons.lang3.math.NumberUtils; // Importieren der NumberUtils-Klasse aus Apache Commons Lang
+import java.io.IOException; // Importieren der IOException Klasse
 
 public class App {
     public String getGreeting() {
@@ -15,5 +17,14 @@ public class App {
         String textNumber = "123";
         int number = NumberUtils.toInt(textNumber);
         System.out.println("Die Zahl ist: " + number);
+
+        try {
+            // ASCII Art greeting
+            String asciiArt = FigletFont.convertOneLine("Hello, World!");
+            System.out.println(asciiArt);
+        } catch (IOException e) {
+            System.out.println("Ein Fehler ist aufgetreten beim Erstellen der ASCII-Kunst.");
+            e.printStackTrace(); // Gibt den Stacktrace des Fehlers aus. Optional, je nach Bedarf.
+        }
     }
 }

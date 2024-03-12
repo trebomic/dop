db006fb8e8ace77110fe4526d82b284223b29e53
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-12 13:53:02 +0100

    Add a second dependency which convert text to numbers

--- Woche 4 Build Tools (Gradle und NPM)/MyFirstBuild/app/src/main/java/myfirstbuild/App.java
+++ Woche 4 Build Tools (Gradle und NPM)/MyFirstBuild/app/src/main/java/myfirstbuild/App.java
@@ -1,6 +1,6 @@
 package myfirstbuild;
-import com.github.lalyos.jfiglet.FigletFont;
-import java.io.IOException; // Importieren der IOException Klasse
+
+import org.apache.commons.lang3.math.NumberUtils; // Importieren der NumberUtils-Klasse aus Apache Commons Lang
 
 public class App {
     public String getGreeting() {
@@ -11,13 +11,9 @@ public class App {
         // Original greeting
         System.out.println(new App().getGreeting());
 
-        try {
-            // ASCII Art greeting
-            String asciiArt = FigletFont.convertOneLine("Hello, World!");
-            System.out.println(asciiArt);
-        } catch (IOException e) {
-            System.out.println("Ein Fehler ist aufgetreten beim Erstellen der ASCII-Kunst.");
-            e.printStackTrace(); // Gibt den Stacktrace des Fehlers aus. Optional, je nach Bedarf.
-        }
+        // Text in Zahl umwandeln
+        String textNumber = "123";
+        int number = NumberUtils.toInt(textNumber);
+        System.out.println("Die Zahl ist: " + number);
     }
 }

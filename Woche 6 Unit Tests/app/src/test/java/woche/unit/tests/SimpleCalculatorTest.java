2f2622e6b47726d444d254c32af8224e59bb5ad2
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 14:43:22 +0100

    Add testSubtract

--- Woche 6 Unit Tests/app/src/test/java/woche/unit/tests/SimpleCalculatorTest.java
+++ Woche 6 Unit Tests/app/src/test/java/woche/unit/tests/SimpleCalculatorTest.java
@@ -9,4 +9,9 @@ public class SimpleCalculatorTest {
     public void testAddition() {
         assertEquals(5, SimpleCalculator.add(2, 3));
     }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(1, SimpleCalculator.subtract(3, 2));
+    }
 }

4a8fb500c14e48c0810e03e67ab8cbbdd07ba35d
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 14:53:41 +0100

    Add divide method and check if test is successful (yes, it is)

--- Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/SimpleCalculator.java
+++ Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/SimpleCalculator.java
@@ -12,4 +12,10 @@ public class SimpleCalculator {
     public static int multiply(int a, int b) {
         return a * b;
     }
+
+    public static int divide(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Division by zero");
+        return a / b;
+    }
 }

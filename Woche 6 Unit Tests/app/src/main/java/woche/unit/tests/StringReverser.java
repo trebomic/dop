1deaf2451dbee24033b85a9c257d2245bccfedd5
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 14:11:48 +0100

    Delete tests and function to allow test to fail

--- Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/StringReverser.java
+++ Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/StringReverser.java
@@ -2,14 +2,5 @@ package woche.unit.tests;
 
 public class StringReverser {
 
-    public String reverse(String input) {
-        // Überprüft, ob der Eingabestring null ist, um eine NullPointerException zu
-        // vermeiden
-        if (input == null) {
-            return null;
-        }
-
-        // Verwendet StringBuilder, um den Eingabestring umzukehren
-        return new StringBuilder(input).reverse().toString();
-    }
+   
 }

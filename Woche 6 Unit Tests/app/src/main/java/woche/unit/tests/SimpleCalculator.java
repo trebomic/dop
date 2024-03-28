5a46d12677148d37a92a5f9f629181594b180f4c
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 14:40:22 +0100

    Add addition method and check if test is successful (yes, it is)

--- Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/SimpleCalculator.java
+++ Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/SimpleCalculator.java
@@ -1,5 +1,7 @@
 package woche.unit.tests;
 
 public class SimpleCalculator {
-    
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
--- Woche 6 Unit Tests/app/src/test/java/woche/unit/tests/SimpleCalculatorTest.java
+++ Woche 6 Unit Tests/app/src/test/java/woche/unit/tests/SimpleCalculatorTest.java
@@ -7,6 +7,6 @@ import org.junit.jupiter.api.Test;
 public class SimpleCalculatorTest {
     @Test
     public void testAddition() {
-        assertEquals(5, Calculator.add(2, 3));
+        assertEquals(5, SimpleCalculator.add(2, 3));
     }
 }

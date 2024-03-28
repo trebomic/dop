fc261c7c6755a533b961a18e83d25a02325b191d
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 14:48:35 +0100

    Add multiply method and check if test is successful (yes, it is)

--- Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/SimpleCalculator.java
+++ Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/SimpleCalculator.java
@@ -8,4 +8,8 @@ public class SimpleCalculator {
     public static int subtract(int a, int b) {
         return a - b;
     }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 }

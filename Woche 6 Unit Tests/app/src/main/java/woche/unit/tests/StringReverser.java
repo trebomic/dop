1169cfa9e95f8bc6e8bd34e65e4190aa03184985
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 12:54:22 +0100

    Add function and change version

--- Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/StringReverser.java
+++ Woche 6 Unit Tests/app/src/main/java/woche/unit/tests/StringReverser.java
@@ -3,7 +3,13 @@ package woche.unit.tests;
 public class StringReverser {
 
     public String reverse(String input) {
-        // Ein einfacher Weg, einen String in Java umzukehren
+        // Überprüft, ob der Eingabestring null ist, um eine NullPointerException zu
+        // vermeiden
+        if (input == null) {
+            return null;
+        }
+
+        // Verwendet StringBuilder, um den Eingabestring umzukehren
         return new StringBuilder(input).reverse().toString();
     }
 }

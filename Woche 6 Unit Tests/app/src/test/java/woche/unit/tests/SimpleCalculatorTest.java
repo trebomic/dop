84984751310cf2c83891c56f8ec0631695d2f2b2
trebomic <58138261+trebomic@users.noreply.github.com>
2024-03-28 14:26:34 +0100

    Add testAddition

--- Woche 6 Unit Tests/app/src/test/java/woche/unit/tests/SimpleCalculatorTest.java
+++ Woche 6 Unit Tests/app/src/test/java/woche/unit/tests/SimpleCalculatorTest.java
@@ -1,5 +1,12 @@
 package woche.unit.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class SimpleCalculatorTest {
-    
+    @Test
+    public void testAddition() {
+        assertEquals(5, Calculator.add(2, 3));
+    }
 }
